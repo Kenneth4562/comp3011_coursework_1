5998ff2fe55e1d73f5e1d03c73bc3facbdb53053
Kenneth Lo <kenzjlo@hotmail.com>
2026-03-10 16:41:19 +0000

    databases created + registered

--- coursework_one/tfl_updates/admin.py
+++ coursework_one/tfl_updates/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Stop, Line, UserRoute, UserStation, ArrivalRecord, UserIncident
 
 # Register your models here.
+admin.site.register(Stop)
+admin.site.register(Line)
+admin.site.register(UserRoute)
+admin.site.register(UserStation)
+admin.site.register(ArrivalRecord)
+admin.site.register(UserIncident)

65291f1f27420d0b101fb4c4637fd4ac72404522
Kenneth Lo <kenzjlo@hotmail.com>
2026-03-12 22:33:21 +0000

    Updated annotations
    Swagger documentation explains each CRUD operation for Favourite User Routes, User Stops and Incident reporting

--- coursework_one/tfl_updates/views.py
+++ coursework_one/tfl_updates/views.py
@@ -11,6 +11,8 @@ from .serializers import (
 )
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework.filters import OrderingFilter
+from drf_yasg.utils import swagger_auto_schema
+from drf_yasg import openapi
 
 # Create your views here.
 """ def HandleRegisterRequest(request):
@@ -20,32 +22,144 @@ class UserRouteListCreateView(generics.ListCreateAPIView):
     queryset = UserRoute.objects.all()
     serializer_class = UserRouteSerializer
 
+    @swagger_auto_schema(
+        operation_description="Retrieve all user routes",
+        tags=["User Routes"]
+    )
+    def get(self, request, *args, **kwargs):
+        return super().get(request, *args, **kwargs)
+
+    @swagger_auto_schema(
+        operation_description="Create a new user route",
+        tags=["User Routes"]
+    )
+    def post(self, request, *args, **kwargs):
+        return super().post(request, *args, **kwargs)
+
 class UserRouteDetailView(generics.RetrieveUpdateDestroyAPIView):
     queryset = UserRoute.objects.all()
     serializer_class = UserRouteSerializer
+
+    @swagger_auto_schema(
+        operation_description="Retrieve a specific user route",
+        tags=["User Routes"]
+    )
+    def get(self, request, *args, **kwargs):
+        return super().get(request, *args, **kwargs)
+
+    @swagger_auto_schema(
+        operation_description="Update a specific user route",
+        tags=["User Routes"]
+    )
+    def put(self, request, *args, **kwargs):
+        return super().put(request, *args, **kwargs)
+
+    @swagger_auto_schema(
+        operation_description="Partially update a specific user route",
+        tags=["User Routes"]
+    )
+    def patch(self, request, *args, **kwargs):
+        return super().patch(request, *args, **kwargs)
+
+    @swagger_auto_schema(
+        operation_description="Delete a specific user route",
+        tags=["User Routes"]
+    )
+    def delete(self, request, *args, **kwargs):
+        return super().delete(request, *args, **kwargs)
 
 class UserStationListCreateView(generics.ListCreateAPIView):
     queryset = UserStation.objects.all()
     serializer_class = UserStationSerializer
 
+    @swagger_auto_schema(
+        operation_description="Retrieve all favourite stations for all users",
+        tags=["User Stations"]
+    )
+    def get(self, request, *args, **kwargs):
+        return super().get(request, *args, **kwargs)
+
+    @swagger_auto_schema(
+        operation_description="Add a new favourite station for a user",
+        tags=["User Stations"]
+    )
+    def post(self, request, *args, **kwargs):
+        return super().post(request, *args, **kwargs)
+
 class UserStationDetailView(generics.RetrieveDestroyAPIView):
     queryset = UserStation.objects.all()
     serializer_class = UserStationSerializer
+
+    @swagger_auto_schema(
+        operation_description="Retrieve a specific favourite station",
+        tags=["User Stations"]
+    )
+    def get(self, request, *args, **kwargs):
+        return super().get(request, *args, **kwargs)
+
+    @swagger_auto_schema(
+        operation_description="Delete a favourite station",
+        tags=["User Stations"]
+    )
+    def delete(self, request, *args, **kwargs):
+        return super().delete(request, *args, **kwargs)
 
 class UserIncidentListCreateView(generics.ListCreateAPIView):
     queryset = UserIncident.objects.all()
     serializer_class = UserIncidentSerializer
 
+    @swagger_auto_schema(
+        operation_description="Retrieve all user-reported incidents",
+        tags=["User Incidents"]
+    )
+    def get(self, request, *args, **kwargs):
+        return super().get(request, *args, **kwargs)
+
+    @swagger_auto_schema(
+        operation_description="Create a new user-reported incident",
+        tags=["User Incidents"]
+    )
+    def post(self, request, *args, **kwargs):
+        return super().post(request, *args, **kwargs)
+
 class UserIncidentDetailView(generics.RetrieveUpdateDestroyAPIView):
     queryset = UserIncident.objects.all()
     serializer_class = UserIncidentSerializer
+
+    @swagger_auto_schema(
+        operation_description="Retrieve a specific incident",
+        tags=["User Incidents"]
+    )
+    def get(self, request, *args, **kwargs):
+        return super().get(request, *args, **kwargs)
+
+    @swagger_auto_schema(
+        operation_description="Update a specific incident",
+        tags=["User Incidents"]
+    )
+    def put(self, request, *args, **kwargs):
+        return super().put(request, *args, **kwargs)
+
+    @swagger_auto_schema(
+        operation_description="Partially update a specific incident",
+        tags=["User Incidents"]
+    )
+    def patch(self, request, *args, **kwargs):
+        return super().patch(request, *args, **kwargs)
+
+    @swagger_auto_schema(
+        operation_description="Delete a specific incident",
+        tags=["User Incidents"]
+    )
+    def delete(self, request, *args, **kwargs):
+        return super().delete(request, *args, **kwargs)
 
 class StopListView(generics.ListAPIView):
     queryset = Stop.objects.all()
     serializer_class = StopSerializer
     filter_backends = [DjangoFilterBackend]
     filterset_fields = ["mode"]
-
+    swagger_tags = ['Stops']
 
 class LineListView(generics.ListAPIView):
     """
@@ -53,3 +167,4 @@ class LineListView(generics.ListAPIView):
     """
     queryset = Line.objects.all()
     serializer_class = LineSerializer
+    swagger_tags = ['Lines']
